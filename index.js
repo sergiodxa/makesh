b92488a1973f52079b6086c1f081fdccd4990047
Sergio Daniel Xalambrí <daniel@mejorando.la>
2015-09-07 17:22:07 -0500

    [add] main file

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import program from 'commander';
+
+import makeApp from './app';
+
+program
+  .version('1.0.0')
+  .option('--path [path]', 'set path to create files');
+
+program
+  .command('app')
+  .description('Initialize an application')
+  .action(makeApp);
+
+program.parse(process.argv);
